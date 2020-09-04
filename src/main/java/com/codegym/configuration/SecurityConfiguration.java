1f971ec36a3f84feccb7e1a09b46238f1503e30d
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-09-04 15:27:11 +0700

    OK

---
+++ src/main/java/com/codegym/configuration/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.codegym.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomSuccessHandler customSuccessHandler;
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("123").roles("USER");
+        auth.inMemoryAuthentication().withUser("dba").password("123").roles("DBA", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("123").roles("ADMIN", "DBA", "USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/home").access("hasRole('USER')")
+                .antMatchers("/dba/**").access("hasRole('DBA')")
+                .antMatchers("/**").access("hasRole('ADMIN')")
+                .and().formLogin().successHandler(customSuccessHandler)
+                .usernameParameter("ssoId").passwordParameter("password")
+                .and().csrf()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
+}
